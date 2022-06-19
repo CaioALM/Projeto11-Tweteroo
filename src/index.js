236a5eb22fe456f1bf9bd3bbb2493e57208eedbb
Caio <caiosobreira77@gmail.com>
2022-06-18 21:32:42 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,23 @@ app.use(cors())
 const users = []
 const tweets = []
 
+function newUser(user) {
+    return {
+        username: user.username,
+        avatar: user.avatar
+    }
+}
+
 app.post('/sign-up', (req, res) => {
 
+})
+
+app.post('/tweets', (req, res) => {
 
 })
+
+app.get('/tweets', (req, res) => {
+    
+})
+
+app.listen(5000)
