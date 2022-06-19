b1b16e7faf8f9e75a505198f34e012e5e914b261
Caio <caiosobreira77@gmail.com>
2022-06-18 21:03:45 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+
+
+const app = express()
+app.use(cors())
+
+const users = []
+const tweets = []
+
+app.post('/sign-up', (req, res) => {
+
+
+})
